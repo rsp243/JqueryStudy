9f2faca9f72545be68e285b10de5273e23531f98
rsp243 <you@example.comreshetov.semjon@gmail.com>
2020-12-25 01:27:25 +0400

    25 lesson

---
+++ text/js/main.js
@@ -0,0 +1,32 @@
+$(function(){
+	alert($('p').length);
+});
+	//$('.icons img').each(function(){
+	//	if ($(this).attr('src') == 'img/icon3.png'){
+	//		$(this).fadeOut(1000);
+	//	}
+	//});
+
+	//function change(element, newAttr, newWalue){
+	//	var className = "." + element;
+	//	$(className).attr(newAttr, newWalue);
+	//}
+	//change("logo", "title", "Новая подсказка");
+
+	//$('.mainText').fadeTo(4000, 0.25);
+	//$('.mainText').slideUp(2000).slideDown(3000).fadeTo(2000, 1);
+
+	//$('.mainText').fadeTo(4000, 0.25).fadeTo(2000, 1);	
+
+	//function elemOut (element, time) {
+	//	if (time>5000 || time<1000){
+	//		return false;
+	//	} else {
+	//		var className = "." + element;
+	//		alert(className);
+	//		$(className).fadeOut(time);
+	//	}
+	//}
+	//elemOut('mainText', 5000);
+
+	//$('.mainText').fadeOut(2000).fadeIn(3000);
